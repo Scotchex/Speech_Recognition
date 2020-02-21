cd3014040dbf08a362ff75eb31e114f4f7545535
2b2b2b <59747023+Scotchex@users.noreply.github.com>
2020-02-21 22:04:30 +0100

    Update voice recognition.py

--- voice recognition.py
+++ voice recognition.py
@@ -1,28 +1,30 @@
-char userInput;
-
-void setup(){
-  pinMode(3, OUTPUT);
-  Serial.begin(9600);                        
-
-}
-
-void loop(){
-
-while(Serial.available()> 0){ 
-    
-    userInput = Serial.read();             
-      
-      if(userInput == '1'){                  
-        digitalWrite(3, HIGH);
-        
-      }
-       else {
-          digitalWrite(3, LOW);
-        }
-
-       
-                        
+import speech_recognition as sr
+import serial
+Arduino_Serial = serial.Serial('COM3', 9600)
+value = 0
+while True :
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say Something :")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
             
-      
-      } 
-  }
+            if text == ("your text"):
+                value = "1"
+                b = value.encode()
+                Arduino_Serial.write(b)
+            elif text == ("your text"):
+                value = "2"
+                b = value.encode()
+                Arduino_Serial.write(b)
+                
+            else :
+                print("You said : {}".format(text))
+    
+        except:
+            print("Could not recognize what you said")
+
+
+##written by 2b2b2b
